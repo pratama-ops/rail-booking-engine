735cef6980f98db72144059af1d7326675d2e040
enzie <mulyaenziepratama@gmail.com>
2026-06-24 11:38:57 +0700

    setup redis connection bullMQ

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,23 @@ import { CarModule } from './car/car.module';
 import { RouteModule } from './route/route.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { BookingModule } from './booking/booking.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [PrismaModule, TrainModule, CarModule, RouteModule, ScheduleModule, BookingModule],
+  imports: [
+    PrismaModule,
+    TrainModule,
+    CarModule,
+    RouteModule,
+    ScheduleModule,
+    BookingModule,
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST ?? 'localhost',
+        port: parseInt(process.env.REDIS_PORT ?? '6379'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
+import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ExpressAdapter } from '@bull-board/express';
+import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { Queue } from 'bullmq';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/queue');
+
+  createBullBoard({
+    queues: [
+      new BullMQAdapter(
+        new Queue('Rail-Booking-Engine', {
+          connection: {
+            host: process.env.REDIS_HOST ?? 'localhost',
+            port: parseInt(process.env.REDIS_PORT ?? '6379'),
+          },
+        }),
+      ),
+    ],
+    serverAdapter,
+  });
+
+  app.use('/queue', serverAdapter.getRouter());
   
   const config = new DocumentBuilder()
     .setTitle('Rail-Booking-Engine')
